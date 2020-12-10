87fc52409c3a0528f9125e6d966a03b59f1e9566
Utham Kumar <uthamkmr@gmail.com>
2020-12-10 13:20:57 +0530

    Data driver interface

--- src/main/java/com/ep/data/DataDriver.java
+++ src/main/java/com/ep/data/DataDriver.java
@@ -1,8 +1,10 @@
 package com.ep.data;
 
+import java.util.List;
+
 public interface DataDriver {
 
-    public void readSource(String path);
+    public <T> List<T> readSource(String path);
 
     public <T> void writeSource(String path,T t );
 
