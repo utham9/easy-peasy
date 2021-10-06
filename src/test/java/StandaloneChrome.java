1070054cb4e567561a16c887d17abe39071b5e5d
Utham Kumar <uthamkmr@gmail.com>
2021-10-06 15:10:14 +0000

    Docker Config

---
+++ src/test/java/StandaloneChrome.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class StandaloneChrome {
+
+    @Test
+    public void sout() {
+        System.out.println("*******************SDsds**************8");
+    }
+}
